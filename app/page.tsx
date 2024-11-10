3f5efa23c664fc4aff8d58bd0b2704f738df095c
andykamp <akampenes@gmail.com>
2024-11-10 15:32:54 +0100

    feat: add console.log in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { ComingSoon } from "@/components/coming-soon"
 
 export default function Page() {
+  console.log("HEHE trodde du var smart nå? :D", )
   return <ComingSoon />
 }
