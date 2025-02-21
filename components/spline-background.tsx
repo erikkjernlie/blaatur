b8aecfb3facf3e0e18be83c7bf0158089382d0cc
andykamp <akampenes@gmail.com>
2025-02-21 23:33:24 +0100

    feat: use iframe instad of splinetool

--- components/spline-background.tsx
+++ components/spline-background.tsx
@@ -1,22 +1,28 @@
 // "use client"
 
-import Spline from '@splinetool/react-spline/next';
+// import Spline from '@splinetool/react-spline/next';
 // import Spline from '@splinetool/react-spline';
 
 export function SplineBackground() {
   return (
-    <main 
+    <main
       className="w-screen"
       style={{ height: "100dvh", maxHeight: "100dvh" }}
     >
-      <Spline
-        // scene="https://prod.spline.design/faNCQroi2cRAEYWB/scene.splinecode"
-        // scene="https://prod.spline.design/LCpKdVgTVy9jcUu0/scene.splinecode"
-        // scene="https://prod.spline.design/LCpKdVgTVy9jcUu0/scene.splinecode"
-        // scene="https://prod.spline.design/p2-NlwEFazUYrmM3/scene.splinecode"
-        scene="https://prod.spline.design/rOul41cJ-uZSULGw/scene.splinecode"
-
+      <iframe
+        src="https://my.spline.design/blturorgdeploymentnewcopy-964046ad5e7286958a90c09676ef559a/"
+        className="w-full h-full"
+        style={{ border: "none" }}
+        allowFullScreen
       />
+      {/* <Spline */}
+      {/*   // scene="https://prod.spline.design/faNCQroi2cRAEYWB/scene.splinecode" */}
+      {/*   // scene="https://prod.spline.design/LCpKdVgTVy9jcUu0/scene.splinecode" */}
+      {/*   // scene="https://prod.spline.design/LCpKdVgTVy9jcUu0/scene.splinecode" */}
+      {/*   // scene="https://prod.spline.design/p2-NlwEFazUYrmM3/scene.splinecode" */}
+      {/*   scene="https://prod.spline.design/rOul41cJ-uZSULGw/scene.splinecode" */}
+
+      {/* /> */}
     </main>
   );
 }
